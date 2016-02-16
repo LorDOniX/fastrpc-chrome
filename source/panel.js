64253add695a0dd456df09bd02ea18b22b72ca7b
Roman Makudera <roman.makudera@firma.seznam.cz>
2016-02-16 07:58:47 +0100

    podbarveni requestu + parovani, drobne upravy

--- source/panel.js
+++ source/panel.js
@@ -1,19 +1,26 @@
-var DATA = [];
+var DATA = {};
+var RESPONSES = {};
 
 var formatCallParams = function(data) {
 	var arr = [];
+	var jsonArr = [];
+
 	for (var i=0;i<data.length;i++) {
 		var item = data[i];
+
 		if (item === null) {
 			arr.push("null");
 		} else if (item instanceof Array) {
 			arr.push("[...]");
+			jsonArr.push(item);
 		} else if (typeof(item) == "object") {
-			arr.push("{...}")
+			arr.push("{...}");
+			jsonArr.push(item);
 		} else {
 			arr.push(item);
 		}
 	}
+
 	return "(" + arr.join(", ") + ")";
 }
 
@@ -31,14 +38,16 @@ var formatArrow = function(type) {
 	return node;
 }
 
-var logRow = function(opt) {
+var requestLog = function(opt, conn) {
 	var row = document.createElement("div");
 	row.title = "Click to open in a window";
-	row.setAttribute("data-index", DATA.length);
+	row.style.background = "#9CFF82";
+	row.setAttribute("data-type", "request");
+	row.setAttribute("data-conn", conn);
 
 	opt = opt || {};
 
-	var ar = ["FRPC"].concat(opt.values || []);
+	var ar = [].concat(opt.values || []);
 
 	for (var i=0;i<ar.length;i++) {
 		var item = ar[i];
@@ -49,13 +58,40 @@ var logRow = function(opt) {
 		row.appendChild(document.createTextNode(" "));
 	}
 
-	document.querySelector("#log").appendChild(row);
+	var row2 = document.createElement("div");
+	row2.title = "Click to open in a window";
+	row2.setAttribute("data-type", "response");
+	row2.setAttribute("data-conn", conn);
+	row2.innerHTML = "Loading...";
+
+	var logEl = document.querySelector("#log");
+
+	logEl.appendChild(row);
+	logEl.appendChild(row2);
+
 	document.body.scrollTop = document.body.scrollHeight;
 
-	return row;
+	return row2;
 }
 
-var logRequest = function(data) {
+var responseRow = function(row, opt) {
+	opt = opt || {};
+
+	var ar = [].concat(opt.values || []);
+
+	row.innerHTML = "";
+
+	for (var i=0;i<ar.length;i++) {
+		var item = ar[i];
+		if (!item.nodeType) {
+			item = document.createTextNode(item);
+		}
+		row.appendChild(item);
+		row.appendChild(document.createTextNode(" "));
+	}
+}
+
+var logRequest = function(data, conn) {
 	var arrow = formatArrow(0);
 	var dataItem;
 
@@ -67,48 +103,72 @@ var logRequest = function(data) {
 		method.innerHTML = parsed.method;
 
 		var callParams = formatCallParams(parsed.params);
-		dataItem = { values: [arrow, data.url, method, callParams]};
+		dataItem = { values: ["[request]", arrow, data.url, method, callParams]};
 
-		logRow(dataItem);
-
-		DATA.push({
+		DATA[conn + "_request"] = {
 			data: parsed.params,
 			item: dataItem
-		});
-	} catch (e) {
-		dataItem = { values: [arrow, data.url, formatException(e)]};
-		logRow(dataItem);
+		};
 
-		DATA.push({
+		RESPONSES[conn] = requestLog(dataItem, conn);
+	} catch (e) {
+		dataItem = { values: ["[request]", arrow, data.url, formatException(e)]};
+
+		DATA[conn + "_request"] = {
 			data: e,
 			item: dataItem
-		});
+		};
+
+		RESPONSES[conn] = requestLog(dataItem, conn);
 	}
 }
+
+var humanLength = function(size) {
+	if (size < 1024) {
+		return size + " B";
+	}
+	else if (size >= 1024 && size <= 1024*1024) {
+		size = (size / 1024).toFixed(2);
+
+		return size + " KB";
+	}
+	else {
+		size = (size / (1024 * 1024)).toFixed(2);
+
+		return size + " MB";
+	}
+};
 
 var logResponse = function(data, content, harEntry) {
 	var arrow = formatArrow(1);
 	var dataItem;
+	var conn = harEntry.connection;
 
 	try {
 		var decoded = atob(content);
 		var binary = JAK.Base64.atob(decoded);
 		var parsed = JAK.FRPC.parse(binary);
 
-		dataItem = { values: [arrow, harEntry.request.url, "|" , data.bodySize + " bytes"]};
-		logRow(dataItem);
-		DATA.push({
+		dataItem = { values: ["[response]", arrow, humanLength(data.bodySize)]};
+		dataItem2 = { values: ["[response]", arrow, harEntry.request.url, humanLength(data.bodySize)]};
+
+		DATA[conn + "_response"] = {
 			data: parsed,
-			item: dataItem
-		});
+			item: dataItem2
+		};
+
+		responseRow(RESPONSES[conn], dataItem);
 	} catch (e) {
-		dataItem = { values: [arrow, formatException(e)]};
-		logRow(dataItem);
-		ERRORS.push(content)
-		DATA.push({
+		dataItem = { values: ["[response]", arrow, formatException(e)]};
+		
+		ERRORS.push(content);
+
+		DATA[conn + "_response"] = {
 			data: e,
 			item: dataItem
-		});
+		};
+
+		responseRow(RESPONSES[conn], dataItem);
 	}
 }
 
@@ -123,12 +183,16 @@ var isFRPC = function(headers) {
 
 var processItem = function(harEntry) {
 	var request = harEntry.request;
-	if (isFRPC(request.headers)) { logRequest(request); }
+	var conn = harEntry.connection;
+
+	if (isFRPC(request.headers)) { 
+		logRequest(request, conn);
+	}
 
 	var response = harEntry.response;
 	if (isFRPC(response.headers)) { 
 		harEntry.getContent(function(content) {
-			logResponse(response, content, harEntry); 
+			logResponse(response, content, harEntry)
 		});
 	}
 }
@@ -163,11 +227,16 @@ chrome.devtools.network.onRequestFinished.addListener(processItem);
 chrome.devtools.network.getHAR(processItems);
 
 document.querySelector("#clear").addEventListener("click", function(e) {
-	DATA = [];
+	DATA = {};
+	RESPONSES = {};
 	document.querySelector("#log").innerHTML = "";
+	e.preventDefault();
+	e.stopPropagation();
 });
 
 document.querySelector("#log").addEventListener("click", function(e) {
+	e.preventDefault();
+	e.stopPropagation();
 	var target = e.target;
 	var row = null;
 	while (target.parentNode) {
@@ -176,17 +245,18 @@ document.querySelector("#log").addEventListener("click", function(e) {
 	}
 	if (!row) { return; }
 
-	var index = row.getAttribute("data-index") || 0;
+	var dataType = row.getAttribute("data-type") || "";
+	var dataConn = row.getAttribute("data-conn") || "";
+	var dataKey = dataConn + "_" + dataType;
 
-	var data = DATA[index];
+	if (!dataKey in DATA) return;
+	
+	var data = DATA[dataKey];
+
 	var str = JSON.stringify(data.data, null, "  ");
 	str = str.replace(/</g, "&lt;");
 
 	var w = window.open();
-	/*var jsonDataViewer = $(document.createElement("div"));
-	jsonDataViewer.JSONView(JSON.parse(str));*/
-
-	//w.document.head.innerHTML = '<style>@charset "UTF-8";.jsonview {  font-family: monospace;  font-size: 1.5em;  white-space: pre-wrap; }  .jsonview .prop {    font-weight: bold; }  .jsonview .null {    color: red; }  .jsonview .bool {    color: blue; }  .jsonview .num {    color: blue; }  .jsonview .string {    color: green;    white-space: pre-wrap; }    .jsonview .string.multiline {      display: inline-block;      vertical-align: text-top; }  .jsonview .collapser {    position: absolute;    left: -1em;    cursor: pointer; }  .jsonview .collapsible {    transition: height 1.2s;    transition: width 1.2s; }  .jsonview .collapsible.collapsed {    height: .8em;    width: 1em;    display: inline-block;    overflow: hidden;    margin: 0; }  .jsonview .collapsible.collapsed:before {    content: "…";    width: 1em;    margin-left: .2em; }  .jsonview .collapser.collapsed {    transform: rotate(0deg); }  .jsonview .q {    display: inline-block;    width: 0px;    color: transparent; }  .jsonview li {    position: relative; }  .jsonview ul {    list-style: none;    margin: 0 0 0 2em;    padding: 0; }  .jsonview h1 {    font-size: 1.2em; }</style>';
 
 	w.document.head.innerHTML = '<style>' +
 	'pre { padding: 5px; margin: 5px; font-size: 14px; }' +
@@ -201,7 +271,7 @@ document.querySelector("#log").addEventListener("click", function(e) {
 	p.style.fontSize = "20px";
 
 	var pInfo = document.createElement("span");
-	pInfo.innerHTML = "Request: <strong style='margin: 0 15px;'>FRPC</strong>";
+	pInfo.innerHTML = "";
 	p.appendChild(pInfo);
 
 	data.item.values.forEach(function(i) {
@@ -223,7 +293,6 @@ document.querySelector("#log").addEventListener("click", function(e) {
 	w.document.body.appendChild(p);
 
 	w.document.body.appendChild(document.createElement("hr"));
-	//w.document.body.appendChild(jsonDataViewer[0]);
 
 	var pre = document.createElement("pre");
 	pre.innerHTML = syntaxHighlight(JSON.stringify(data.data, undefined, 4));
